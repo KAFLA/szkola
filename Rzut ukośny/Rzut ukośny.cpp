83f3685370c2321cd2d8dff488a33fab7b619642
KAFLISZ <kaflisz123@outlook.com>
2021-09-30 13:32:02 +0200

    Gotowy program

--- Rzut ukośny/Rzut ukośny.cpp
+++ Rzut ukośny/Rzut ukośny.cpp
@@ -1,11 +1,25 @@
-﻿// Rzut ukośny.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
-//
-
+﻿#define _USE_MATH_DEFINES
 #include <iostream>
+#include <cmath>
 using namespace std;
+
 
 int main()
 {
-    cout << "Hello World!\n";
+	float a, v, angl, Z;
+	const float g = 9.81;
+
+	cout << "Podaj predkosc poczatkowa (V0): "; cin >> v; 
+	cout << "Podaj kat alfa: "; cin >> a;
+
+	angl = a * M_PI / 180;
+
+	cout << "sin(" << a << ") = " << sin(angl) << endl;
+	cout << M_PI << endl;
+
+	Z = (pow(v, 2) * sin(2 * angl)) / g;
+
+	cout << "Zasieg wynosi: " << Z << endl;
+
 }
 
